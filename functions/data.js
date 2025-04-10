2631251cb2572e8a96b83b900c35c5abf3ef37c5
Opper125 <crakb68@gmail.com>
2025-04-11 00:18:20 +0630

    Create data.js

---
+++ functions/data.js
@@ -0,0 +1,6 @@
+const supabase = Supabase.createClient(
+    'https://qdusallinlchgvifxixd.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkdXNhbGxpbmxjaGd2aWZ4aXhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMDExNTcsImV4cCI6MjA1OTg3NzE1N30.Nnbjv3uJTfkwrrFE75PXU23yWATcfPRzQ748ULB8lGc'
+);
+
+window.supabase = supabase;
